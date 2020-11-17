dc8748f05452320d9f8c30015d08502f00361b2f
Pawel Lecki <p.lecki@neurosys.com>
2020-11-17 15:11:38 +0100

    add PaperRow

--- src/pages/AllProducts/AllProducts.tsx
+++ src/pages/AllProducts/AllProducts.tsx
@@ -2,13 +2,31 @@ import React, { FC } from 'react';
 import paper1 from "../../assets/paper1.jpg"
 import paper2 from "../../assets/paper2.jpg"
 import paper3 from "../../assets/paper3.jpg"
-import { Gallery } from "../../components"
-type Props = {
+import { Gallery, PapersRow } from "../../components"
 
-}
+const papersRowConfig = [
+    {
+        background: paper1,
+        title: 'Paper is awesome!',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.'
+    },
+    {
+        background: paper2,
+        title: 'Paper is awesome 2!',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.'
+    },
+    {
+        background: paper3,
+        title: 'Paper is awesome 3!',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet la.'
+    },
+]
 
-const AllProducts: FC<Props> = () =>
-    <Gallery images={[paper1, paper2, paper3]} />
+const AllProducts: FC = () =>
+    <>
+        <Gallery images={[paper1, paper2, paper3]} />
+        <PapersRow config={papersRowConfig} />
+    </>
 
 
 export default AllProducts
